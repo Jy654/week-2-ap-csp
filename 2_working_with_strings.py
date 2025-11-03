3c21d6429518316d9769fff280b4b13616c4110d
Jy654 <JNavarro69@cps.edu>
2025-11-03 09:31:39 -0600

    Update 2_working_with_strings.py

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -4,8 +4,8 @@
 # ----------------------------------------
 
 # Strings are sequences of characters enclosed in quotes (' ' or " ")
-greeting = "Hello" #String data type
-name = "World" #String data type
+greeting = "Hello" #string data type
+name = "World" #string data type
 
 # ----------------------------------------
 # Basic String Operations
@@ -17,32 +17,58 @@ print("Concatenated String:", message)  # Output: Hello World
 
 # ----------------------------------------
 # 2. String Functions
-# ----------------------------------------
+# # ----------------------------------------
 
 phrase = "Python is FUN!"
-name = "Jancarlo"
-phrase2 = "supercagefragislistcious"
-# Convert all characters to lowercase
+myname = "Tito"
+phrase2 = "SUPERCAGEFRAGISLISTCIOUS"
+# # Convert all characters to lowercase
 print("Lowercase:", phrase.lower())  # Output: python is fun!
-print("Name Lowercase:", name.lower())
-print("phrase2 Lowercase:",phrase2.lower)
+print("Name Lowercase:", myname.lower())
+print("Lowercase:", phrase2.lower())
 # # Convert all characters to uppercase
-# print("Uppercase:", phrase.upper())  # Output: PYTHON IS FUN!
-print("Name Uppercase:", name.upper())
-print("Phrase2 Uppercase:", phrase2.upper())
+print("Uppercase:", phrase.upper())  # Output: PYTHON IS FUN!
+print("Name Uppercase:", myname.upper())
+print("Uppercase:", phrase2.upper())
 # # Check if all characters are uppercase
-# print("Is Uppercase?", phrase.isupper())  # Output: False
-
+print("Is Uppercase?", phrase.isupper())  # Output: False
+print("Is Uppercase?", myname.isupper())
+print("Is Uppercase?", phrase2.isupper()) 
 # # Find the length of the string
 # print("Length of phrase:", len(phrase))  # Output: 14
 
+
+
+declaration_of_independence = "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation."
+print(len(declaration_of_independence))
 # # ----------------------------------------
 # # 3. Indexing and Slicing
 # # ----------------------------------------
+chicago_mayor = "Johnson"
+print(chicago_mayor[0])
+print(chicago_mayor[-1])
+print(chicago_mayor[4])
+print(chicago_mayor[ 4 : ])
+# the first number in slicing is inclusing
+# the second number is exclusive
+print(chicago_mayor[0 : -3])
+print(chicago_mayor[1 : -2])
+# When we get one character / letter
+#  its called string indexing
+# when we get a chunk of letters
+# from a string its called
+#  string slicing
 
+phrase3 = "Supercagifragilstic"
+print(phrase3.isupper())
+cut = phrase3[0 : 5]
+print(cut)
+phrase3[5 : 9]
+print(phrase3[-1])
 # # Indexing: Access characters by position (0-based index)
 # print("First character:", phrase[0])  # Output: P
 # print("Last character:", phrase[-1])  # Output: !
+
 
 # # Slicing: Get a range of characters (start inclusive, end exclusive)
 # print("Characters 1 to 4:", phrase[1:4])  # Output: yth
@@ -68,12 +94,13 @@ print("Phrase2 Uppercase:", phrase2.upper())
 # # String Methods
 # # ----------------------------------------
 
-# sentence = "Python is fun to learn"
+sentence = "Python is fun to learn"
 
 # # .split(): Splits the string into a list of words
-# words = sentence.split()
-# print("Split result:", words)
-
+words = sentence.split()
+print("Split result:", words)
+words2 = sentence.join("")
+print(words2)
 # # .format(): Allows inserting values into strings using {}
 # name = "Marvin"
 # age = 35
